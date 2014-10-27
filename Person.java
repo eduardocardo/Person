d69c5b2918684a3cbb4422cefec24cf47d0add35
Eduardo <eduardocardomartinez@gmail.com>
2014-10-27 09:00:21 +0100

    contructor age y metodo getage

---
+++ Person.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private int age;
+   
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(int valorAge)
+    {
+        // initialise instance variables
+        age = valorAge;
+    }
+
+    //Metodo para conocer la edad
+    public int getAge()
+    {
+        // put your code here
+        return age;
+    }
+}
